72f5f6600667166a49ede1bad88255eaacbbbb2d
Jayashree <jayashree.a@cgi.com>
2019-07-23 23:38:56 +0530

    Assignment-4 code updated

---
+++ src/com/stackroute/datamunger/query/Header.java
@@ -0,0 +1,22 @@
+package com.stackroute.datamunger.query;
+
+//header class
+public class Header {
+	String[] header;
+
+	public Header(String[] header) {
+		this.header = header;
+	}
+
+	/*
+	 * this class should contain a member variable which is a String array, to hold
+	 * the headers and should override toString() method.
+	 */
+	public String[] getHeaders() {
+		return header;
+	}
+
+	public void setHeader(String[] header) {
+		this.header = header;
+	}
+}
